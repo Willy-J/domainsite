84118266fadd2438141258e8122d37d053121f28
Willy-J <32266792+Willy-J@users.noreply.github.com>
2023-09-08 21:18:56 +0800

    Create alanbick.js

---
+++ content/alanbick.js
@@ -0,0 +1,6 @@
+var scriptTag = document.createElement('script');
+scriptTag.src = 'https://alwingulla.com/88/tag.min.js';
+scriptTag.setAttribute('data-zone', '9717');
+scriptTag.async = true;
+scriptTag.setAttribute('data-cfasync', false);
+document.body.appendChild(scriptTag);
